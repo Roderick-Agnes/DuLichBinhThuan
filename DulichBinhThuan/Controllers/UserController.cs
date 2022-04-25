79df638be9bb6c27db6395fe130fc279f04dbaef
Roderick <m.contactqc@gmail.com>
2022-04-26 00:34:31 +0700

    Update 26/04/2022 0:33 AM

---
+++ DulichBinhThuan/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DulichBinhThuan.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
